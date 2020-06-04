a0fa47729ff7c7306dc7311b2c14afd51f88b536
hanyunjo <enjongoopee@gmail.com>
2020-06-04 23:36:08 +0900

    shm sem

--- 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
+++ 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
@@ -6,43 +6,21 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
-/*
-void getsem(int semid){
-    struct sembuf getbuf;
-    getbuf.sem_num = 0;
-    getbuf.sem_op = -1;
-    getbuf.sem_flg = SEM_UNDO;
-    if((semop(semid, &getbuf, 1)) == -1){
-        printf("failed get sem\n");
-            exit(1);
-    }
-}
 
-void returnsem(int semid){
-    struct sembuf returnbuf;
-    returnbuf.sem_num = 0;
-    returnbuf.sem_op = 1;
-    returnbuf.sem_flg = SEM_UNDO;
-    if((semop(semid, &returnbuf, 1)) == -1){
-        printf("failed return sem\n");
-            exit(1);
-    }
-}*/
+typedef struct {
+    char text[7];
+    struct sembuf semb;
+} shm_sem;
 
 int main(){
     int shmid, count, semid;
     void *memory = (void *)0;
-    char *text;
-    union semun{
-        int val;
-        struct semid_ds *buf;
-        unsigned short int *array;
-    } arg;
+    //char *text;
+    shm_sem *sha;
+    //struct sembuf semb;
 
-    struct sembuf semb;
-
-    // shared_memory
-    if((shmid = shmget((key_t)1735, sizeof(char)*10, IPC_CREAT|0666)) == -1){
+    // shared memory
+    if((shmid = shmget((key_t)1735, sizeof(shm_sem), IPC_CREAT|0666)) == -1){
         printf("failed shmget func\n");
         exit(1);
     }
@@ -52,47 +30,57 @@ int main(){
         exit(1);
     }
 
-    text = (char *)memory;
+    //text = (char *)memory;
+    sha = (shm_sem *)memory;
 
     // semaphore
-    semb.sem_flg = SEM_UNDO;
-    semb.sem_num = 0;
+    sha->semb.sem_flg = SEM_UNDO;
+    sha->semb.sem_num = 0;
 
     if((semid = semget((key_t)9432, 1, IPC_CREAT|0666)) == -1){
         printf("failed semget func\n");
         exit(1);
     }
 
-    arg.val = 1;
-    if(semctl(semid, 0, SETVAL, arg) == -1){
+    if(semctl(semid, 0, SETVAL, 1) == -1){
         printf("failed semctl1 func\n");
         exit(1);
     }
 
     // function
     for(count = 0; count < 10; count++){
-        //getsem(semid);
-        semb.sem_op = -1;
-        if((semop(semid, &semb, 1)) == -1){
+        sha->semb.sem_op = -1;
+        if((semop(semid, &sha->semb, 1)) == -1){
             printf("failed get sem\n");
             exit(1);
         }
         
-        strcpy(text, "Prog");
+        strcpy(sha->text, "Prog");
         sleep(1);
-        printf("B : %s\n", text);
-        
-        semb.sem_op = 1;
-        if((semop(semid, &semb, 1)) == -1){
+        printf("B : %s\n", sha->text);
+
+        sha->semb.sem_op = 1;
+        if((semop(semid, &sha->semb, 1)) == -1){
             printf("failed get sem\n");
             exit(1);
         }
-        //returnsem(semid);
     }
 
     if(shmdt(memory) == -1){
         printf("failed shmdt func\n");
         exit(1);
+    }
+
+    if(shmctl(smId, IPC_RMID, 0) == -1)
+    {
+        printf("failed remove shm\n");
+        return 0;
+    }
+
+    if(semctl(semId, 0, IPC_RMID, 0) == -1)
+    {
+        fprintf(stderr, "failed remove sem\n");
+        exit(0);
     }
 
     return 0;
