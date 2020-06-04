13c537160a70ab6b61d51cf636ae982b1d5c9fc2
hanyunjo <enjongoopee@gmail.com>
2020-06-04 18:48:14 +0900

    shm sema

---
+++ 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+
+
+int main(){
+    int shmid, count, semid;
+    void *memory = (void *)0;
+    char *text;
+
+    struct sembuf semb;
+
+    if((shmid = shmget((key_t)1735, sizeof(char)*10, IPC_CREAT|0666)) == -1){
+        printf("failed shmget func\n");
+        exit(1);
+    }
+    
+    if((memory = shmat(shmid, (void *)0, 0)) == (void *)-1){
+        printf("failed shmat func\n");
+        exit(1);
+    }
+
+    text = (char *)memory;
+
+    semb.sem_flg = SEM_UNDO;
+    semb.sem_num = 0;
+
+    if((semid = semget((key_t)9432, 1, IPC_CREAT|0666)) == -1){
+        printf("failed semget func\n");
+        exit(1);
+    }
+
+    if(semctl(semid, 0, SETVAL, 1) == -1){
+        printf("failed semctl func\n");
+        exit(1);
+    }
+
+    for(count = 0; count < 10; count++){
+        semb.sem_op = -1;
+        if(semop(semid, &semb, 1) == -1){
+            printf("failed set -1\n");
+            exit(1);
+        }
+        strcpy(text, "Prog");
+        sleep(1);
+        printf("B : %s\n", text);
+        semb.sem_op = 1;
+        if(semop(semid, &semb, 1) == -1){
+            printf("failed set 1\n");
+            exit(1);
+        }
+    }
+
+    return 0;
+}
