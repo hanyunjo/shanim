6da01ec425b5d92e071abe68567a728209752d5e
hanyunjo <enjongoopee@gmail.com>
2020-06-04 22:39:07 +0900

    shm sem

--- 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
+++ 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
@@ -6,11 +6,11 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
-
+/*
 void getsem(int semid){
     struct sembuf getbuf;
     getbuf.sem_num = 0;
-    getbuf.sem_op = 1;
+    getbuf.sem_op = -1;
     getbuf.sem_flg = SEM_UNDO;
     if((semop(semid, &getbuf, 1)) == -1){
         printf("failed get sem\n");
@@ -21,13 +21,13 @@ void getsem(int semid){
 void returnsem(int semid){
     struct sembuf returnbuf;
     returnbuf.sem_num = 0;
-    returnbuf.sem_op = -1;
+    returnbuf.sem_op = 1;
     returnbuf.sem_flg = SEM_UNDO;
     if((semop(semid, &returnbuf, 1)) == -1){
         printf("failed return sem\n");
             exit(1);
     }
-}
+}*/
 
 int main(){
     int shmid, count, semid;
@@ -41,6 +41,7 @@ int main(){
 
     struct sembuf semb;
 
+    // shared_memory
     if((shmid = shmget((key_t)1735, sizeof(char)*10, IPC_CREAT|0666)) == -1){
         printf("failed shmget func\n");
         exit(1);
@@ -53,6 +54,7 @@ int main(){
 
     text = (char *)memory;
 
+    // semaphore
     semb.sem_flg = SEM_UNDO;
     semb.sem_num = 0;
 
@@ -67,18 +69,30 @@ int main(){
         exit(1);
     }
 
+    // function
     for(count = 0; count < 10; count++){
-        getsem(semid);
+        //getsem(semid);
+
+        semb.sem_op = -1;
+        if((semop(semid, &semb, 1)) == -1){
+            printf("failed get sem\n");
+            exit(1);
+        }
         
         strcpy(text, "Prog");
         sleep(1);
         printf("B : %s\n", text);
         
-        returnsem(semid);
+        semb.sem_op = 1;
+        if((semop(semid, &semb, 1)) == -1){
+            printf("failed get sem\n");
+            exit(1);
+        }
+        //returnsem(semid);
     }
 
-    if(semctl(semid, 0, IPC_RMID, arg) == -1){
-        printf("failed semctl2 func\n");
+    if(shmdt(memory) == -1){
+        printf("failed shmdt func\n");
         exit(1);
     }
 
