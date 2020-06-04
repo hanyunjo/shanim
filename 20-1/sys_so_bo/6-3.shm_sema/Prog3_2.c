de6b5a7404fbc437edda2cd0ea48669ae7667d73
hanyunjo <enjongoopee@gmail.com>
2020-06-05 03:38:01 +0900

    shm sem

--- 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
+++ 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
@@ -70,8 +70,8 @@ int main(){
     for(count = 0; count < 10; count++){
         getsem(semid);
         
-        sleep(1);
         strcpy(sha->text, "Prog");
+        sleep(1);
         printf("B : %s\n", sha->text);
 
         returnsem(semid);        
