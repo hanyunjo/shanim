ee884d63b516247271da3d7dcdf06f773c877ada
hanyunjo <enjongoopee@gmail.com>
2020-06-04 21:57:49 +0900

    shm sem

--- 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
+++ 20-1/sys_so_bo/6-3.shm_sema/Prog3_2.c
@@ -7,6 +7,28 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 
+void getsem(int semid){
+    struct sembuf getbuf;
+    getbuf.sem_num = 0;
+    getbuf.sem_op = 1;
+    getbuf.sem_flg = SEM_UNDO;
+    if((semop(semid, &getbuf, 1)) == -1){
+        printf("failed get sem\n");
+            exit(1);
+    }
+}
+
+void returnsem(int semid){
+    struct sembuf returnbuf;
+    returnbuf.sem_num = 0;
+    returnbuf.sem_op = -1;
+    returnbuf.sem_flg = SEM_UNDO;
+    if((semop(semid, &returnbuf, 1)) == -1){
+        printf("failed return sem\n");
+            exit(1);
+    }
+}
+
 int main(){
     int shmid, count, semid;
     void *memory = (void *)0;
@@ -38,26 +60,25 @@ int main(){
         printf("failed semget func\n");
         exit(1);
     }
+
     arg.val = 1;
+    if(semctl(semid, 0, SETVAL, arg) == -1){
+        printf("failed semctl1 func\n");
+        exit(1);
+    }
 
     for(count = 0; count < 10; count++){
-        semb.sem_op = -1;
-        if(semop(semid, &semb, 1) == -1){
-            printf("failed set -1\n");
-            exit(1);
-        }
+        getsem(semid);
+        
         strcpy(text, "Prog");
         sleep(1);
         printf("B : %s\n", text);
-        semb.sem_op = 1;
-        if(semop(semid, &semb, 1) == -1){
-            printf("failed set 1\n");
-            exit(1);
-        }
+        
+        returnsem(semid);
     }
 
     if(semctl(semid, 0, IPC_RMID, arg) == -1){
-        printf("failed semctl func\n");
+        printf("failed semctl2 func\n");
         exit(1);
     }
 
