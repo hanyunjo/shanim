8b3f9ba847ebfefe9262b9990be2651791d4c9f5
hanyunjo <enjongoopee@gmail.com>
2020-06-07 23:01:36 +0900

    IO

--- 20-1/sys_so_bo/IO/file_locking.c
+++ 20-1/sys_so_bo/IO/file_locking.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 
-#define buf_size 100
+#define buf_size 9
 
-int result[10];
+int real = 0;
+char result[10][11];
 
 int get_w_lock(int fd);
 int get_r_lock(int fd);
 int unlock(int fd);
-void *function(void *i);
+void *function1();
+void *function2();
 
 int main(){
 
     pthread_t tid1, tid2;
 
-    pthread_create(&tid1, NULL, function, (void *)1);
-    pthread_create(&tid2, NULL, function, (void *)2);
+    pthread_create(&tid1, NULL, function1, NULL);
+    pthread_create(&tid2, NULL, function2, NULL);
 
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
@@ -26,40 +29,47 @@ int main(){
     return 0;
 }
 
-void *function(void *i){
-    int fd, num, tmp;
-    char *name;
+void *function1(){
+    int fd, tmp2;
+    char rbuf[buf_size];
     int fir, sec, thir;
-
-    num = *(int *)i;
 
     if((fd = open("data.txt", O_CREAT|O_RDWR)) == -1){
         printf("file open error\n");
         exit(1);
     }
 
-    tmp = 0;
-    if(num == 1){
-        if(get_r_lock(fd) == -1){
-            printf("file read_lock error\n");
-            exit(1);
-        }
+    tmp2 = 0;
 
-        while(fscanf(fd, "%d %d %d", &fir, &sec, &thir) != EOF){
-            if((fir + sec + thir) > 150) result[tmp] = 1;
-            else result[tmp] = 0;
-        }
+    memset(rbuf, 0x00, buf_size);
+
+    if(get_r_lock(fd) == -1){
+        printf("file read_lock error\n");
+        exit(1);
     }
-    else if(num == 2){
-        sleep(1);
-        if(get_w_lock(fd) == -1){
-            printf("file write_lock error\n");
-            exit(1);
+    
+    while(read(fd, rbuf, buf_size) > 8){
+        if((int)strlen(rbuf) == 9) rbuf[(int)strlen(rbuf)-1] = '\0';
+        
+        char *num = strtok(rbuf, " ");
+        fir = atoi(num);
+        while(num != NULL){
+            num = strtok(NULL, " ");
+            if(tmp2 == 0){
+                sec = atoi(num);
+                tmp2++;
+            }
+            else if(tmp2 == 1) {
+                thir = atoi(num);
+                break;
+            }
         }
+        tmp2 = 0;
 
-        while(tmp < 10){
-            fprintf(fd, "%d %d %d %d\n", fir, sec, thir, result[tmp]);
-        }
+        if((fir + sec + thir) > 150) sprintf(result[real], "%d %d %d 1\n", fir, sec, thir);
+        else sprintf(result[real], "%d %d %d 0\n", fir, sec, thir);
+
+        real++;
     }
 
     if(unlock(fd) == -1){
@@ -68,7 +78,31 @@ void *function(void *i){
     }
 
     close(fd);
+}
+void *function2(){
+    int fd, tmp;
 
+    if((fd = open("data.txt", O_CREAT|O_RDWR)) == -1){
+        printf("file open error\n");
+        exit(1);
+    }
+
+    tmp = 0;
+    sleep(1);
+
+    if(get_w_lock(fd) == -1){
+        printf("file write_lock error\n");
+        exit(1);
+    }
+
+    write(fd, result[tmp], strlen(result[tmp]));
+
+    if(unlock(fd) == -1){
+        printf("file unlock error\n");
+        exit(1);
+    }
+
+    close(fd);
 }
 
 int get_w_lock(int fd){
