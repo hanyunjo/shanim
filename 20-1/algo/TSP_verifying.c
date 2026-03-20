31a446b0a57b794b39b22ad7b4544452bd29617c
enjongoopee <enjongoopee@gmail.com>
2026-03-20 15:09:26 +0900

    file organization

--- 20-1/algo/TSP_verifying.c
+++
@@ -1,6 +0,0 @@
-bool verify(weighted_digraph G, number d, claimed_tour S){
-    if(S is a tour && the total weight of the edges in S is <= d)
-        return ture;
-    else
-        return false;
-}
